d14bf0a128526c76d0950365083f3941da451a89
Gunn <31373828+KPGunner@users.noreply.github.com>
2018-11-04 14:00:51 -0500

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -3,17 +3,20 @@ import pandas as pd
 import urllib.request
 import arrow
 
+# Pull current date. Shift one day ahead to see if ISS is coming tomorrow.
 day = arrow.utcnow().shift(days=+1).format('ddd MMM D')
-
 print(day)
 
+# Establish page to scrape information from.
 page = urllib.request.urlopen('https://spotthestation.nasa.gov/sightings/view.cfm?\
 country=United_States&region=North_Carolina&city=New_Bern#.W83_6mhKiUm')
 soup = bs.BeautifulSoup(page, 'lxml')
 
+# Find the table with the ISS location information.
 table = soup.find('table')
 rows = table.find_all('tr')
 
+# Pull every row in the table and add it to the table.
 tab = []
 for tr in rows:
     td = tr.find_all('td')
@@ -21,22 +24,25 @@ for tr in rows:
     if row:
         tab.append(row)
 
+# Put the table in a Pandas DataFrame and establish column names.
 df = pd.DataFrame(tab, columns=['Date', 'Visible', 'Max Height', 'Appears', 'Disappears', 'Share'])
 del df['Share']
-#print(df)
 
-new = df['Date'].str.split(',', n = 1, expand=True)
-df['Day']=new[0]
-df['Time']=new[1]
+# Split the "Date" column to separate the date and time. Establish new columns and arrange
+new = df['Date'].str.split(',', n=1, expand=True)
+df['Day'] = new[0]
+df['Time'] = new[1]
 df.drop(columns=['Date'], inplace=True)
 iss = pd.DataFrame(df, columns=['Day', 'Time', 'Visible', 'Max Height', 'Appears', 'Disappears'])
-#print(iss)
 
+
+# Iterate through the DataFrame to find if tomorrow's date is included, parse row info for message composition.
 def message():
-    for row in iss.itertuples():
-        if day in row:
-            return'\nISS can be seen on ' + row.Day + ' at' + row.Time + '.' \
-            + ' It will appear at ' + row.Appears + ' and disappear at ' + row.Disappears + \
-            '.' +' It will be visible for ' + row.Visible + '.'
+    for row_info in iss.itertuples():
+        if day in row_info:
+            return '\nISS can be seen on ' + row_info.Day + ' at' + row_info.Time + '.' \
+                   + ' It will appear at ' + row_info.Appears + ' and disappear at ' + row_info.Disappears + \
+                   '.' + ' It will be visible for ' + row_info.Visible + '.'
+
 
 (message())
